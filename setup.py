94b0d877a4fc0c754ede618db477d54b3eee6e0c
Alex Crocker <35582902+crockeraw@users.noreply.github.com>
2022-08-17 16:29:21 -0400

    create setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+version = '1.0.0'
+install_requires=["Bio","numpy"]
+
+setup(
+    name='codonOptimizer',
+    version=version,
+    description="Tools to identify preferred codons and codon optimize coding sequences",
+    keywords='codon',
+    author='Alex Crocker',
+    author_email='alex.w.crocker@gmail.com',
+    python_requires='>=3.3',
+    install_requires=install_requires,
+)
